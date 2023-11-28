7d54bb6de794a9482fe3d8dc8f760364c99b9344
Hrushikesh41 <hrushikesh.kok@gmail.com>
2023-11-28 12:14:46 +0530

    Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Matrix from './pages/Matrix/Matrix'
-// import DoubleMatrix from './pages/newMatrix/Doublematrix'
+import DoubleMatrix from './pages/newMatrix/DoubleMatrix'
 import TripleMatrix from './pages/TripleMatrix/TripleMatrix'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -13,8 +13,8 @@ function App() {
         <Routes>
          
           <Route exact path='/' element={<Matrix />} />
+          <Route exact path='/2X3' element={<DoubleMatrix />} />
           <Route exact path='/3X3' element={<TripleMatrix />} />
-          {/* <Route exact path='/2X3' element={<DoubleMatrix />} /> */}
         </Routes>
       </BrowserRouter>
     </>
